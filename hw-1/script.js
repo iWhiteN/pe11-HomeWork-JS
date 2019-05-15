dc0c05e4210067b0dfa33c1bf03d19890244e91d
Roman Loboda <whiten.ncs@gmail.com>
2019-05-15 13:39:57 +0300

    change hw

--- hw-1/script.js
+++ hw-1/script.js
@@ -1,25 +1,38 @@
-let userName = prompt('What is your name?');
-let userAge = prompt('How old are you?');
-
-// Clear NaN
-userName = !userName ? '' : userName;
-userAge = !userAge ? '' : userAge;
-
-// Repeat input
-if (!userName || !userAge || isNaN(userAge)) {
-	userName = prompt('What is your name?', userName);
-	userAge = prompt('How old are you?', userAge);
-}
+let userName = prompt('What is your name?'),
+	userAge = prompt('How old are you?')
 
 // Correct input
-if (userName && userAge && !isNaN(userAge)) {
-	if (userAge < 18) {
+if (userName && !isNaN(parseInt(userAge))) {
+
+	if (parseInt(userAge) < 18) {
 		alert('You are not allowed to visit this website');
-	} else if (+userAge > 17 && +userAge < 23) {
+
+	} else if (parseInt(userAge) > 17 && parseInt(userAge) < 23) {
 		// Accept auth
 		let authUser = confirm('Are you sure you want to continue?');
 		authUser ? alert(`Welcome ${userName}`) : alert('You are not allowed to visit this website');
-	} else if (+userAge > 22) {
+
+	} else if (parseInt(userAge) > 22) {
 		alert(`Welcome ${userName}`);
+	}
+
+// Repeat input
+} else if (userName != null && userAge != null) {
+	userName = prompt('Invalid input, please re-enter data \nWhat is your name?', userName);
+	userAge = prompt('Invalid input, please re-enter data \nHow old are you?', userAge);
+
+	if (userName && !isNaN(parseInt(userAge))) {
+
+		if (parseInt(userAge) < 18) {
+			alert('You are not allowed to visit this website');
+	
+		} else if (parseInt(userAge) > 17 && parseInt(userAge) < 23) {
+			// Accept auth
+			let authUser = confirm('Are you sure you want to continue?');
+			authUser ? alert(`Welcome ${userName}`) : alert('You are not allowed to visit this website');
+	
+		} else if (parseInt(userAge) > 22) {
+			alert(`Welcome ${userName}`);
+		}
 	}
 }
