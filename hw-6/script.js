022b6ec21e8aee0acc1fd4b9192e5ba1b49ae216
Roman Loboda <whiten.ncs@gmail.com>
2019-06-04 15:34:08 +0300

    finish hw

---
+++ hw-6/script.js
@@ -0,0 +1,16 @@
+﻿const filterBy = (arr, type) => {
+	const newArr = [];
+
+	arr.forEach(e => {
+		if ( typeof(e) != type) {
+			newArr.push(e)
+		}
+	})
+
+	// Черещ filter
+	/*
+	arr.filter(e => typeof(e) != type)
+	*/
+
+	return newArr
+}
