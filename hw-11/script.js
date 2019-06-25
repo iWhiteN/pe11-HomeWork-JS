40b17e4575a35be1faf4a152a31704a1f7900133
Roman Loboda <whiten.ncs@gmail.com>
2019-06-25 13:48:15 +0300

    end hw-11

---
+++ hw-11/script.js
@@ -0,0 +1,14 @@
+﻿const btns = document.getElementById("btn-wrapper").children;
+let lastBtn;
+
+document.addEventListener("keyup", e =>{
+    for (let i = 0; i < btns.length; i++) {
+        if (btns[i].innerText.toLowerCase() === e.key.toLowerCase()) {
+            
+            lastBtn ? lastBtn.style.backgroundColor = "#33333A" : null;
+            btns[i].style.backgroundColor = "blue";
+            lastBtn = btns[i];
+        }
+    }
+    
+})
