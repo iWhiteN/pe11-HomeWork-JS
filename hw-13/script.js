dd80e59e9705616b187380b8c5bdaf89bb63694e
WhiteN <whiten.ncs@gmail.com>
2019-06-29 14:12:16 +0300

    end hw 14

--- hw-13/script.js
+++ hw-13/script.js
@@ -1,7 +1,7 @@
 const changeTheme = document.getElementById('change_theme');
 const text = document.querySelectorAll(".heder_logo_name, .navbar a, .change_theme a, .latest-news_slogo, .latest-news_btn, .copiryting-text, .copiryting-logo_title");
 const themeText = document.querySelectorAll(".most-popular, .top-rated_title, .hot-news_title, .our-most-popular_slogo");
-const bcg = document.querySelectorAll(".most-popular-block, .top-rated, .hot-news")
+const bcg = document.querySelectorAll(".most-popular-block, .top-rated, .hot-news");
 
 const changeThemeAll = e => {
     text.forEach(e => e.classList.toggle("new_theme_color"));
---
+++ hw-14/script.js
@@ -0,0 +1,17 @@
+﻿const $tabs = $(".tabs-title");
+
+let $activeItem = $('.active'),
+    $activeContent = $('.active-content');
+
+$tabs.click("click", e => {
+    // Добавление активности табу
+    $activeItem.toggleClass('active');
+    $(e.currentTarget).toggleClass('active');
+    $activeItem = $(e.currentTarget);
+
+    // Отображение текста по табу
+    $activeContent.toggleClass('active-content');
+    let $targetContent = $(`.tabs-content-item[data-name="${$activeItem.data("name")}"]`);
+    $targetContent.toggleClass('active-content');
+    $activeContent = $targetContent;
+})
